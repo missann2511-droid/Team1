69e355dd094ef92676f4f52e82cf51a030e03655
missann2511-droid <miss.ann25.11@gmail.com>
2026-06-16 21:09:54 +0000

    main chenges (remove of '.' before from

--- backend/main.py
+++ backend/main.py
@@ -17,7 +17,7 @@ import pandas as pd
 from fastapi import FastAPI, HTTPException
 from fastapi.middleware.cors import CORSMiddleware
 
-from .schemas import (
+from schemas import (
     BatchPredictRequest,
     BatchPredictResponse,
     HealthResponse,
